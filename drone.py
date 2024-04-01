5af655983b314b9e878493323587ce387d0da7b8
Bruna Marques <brunabrasilmarques@gmail.com>
2024-04-01 19:19:57 +0100

    start presence manager

--- drone.py
+++ drone.py
@@ -3,6 +3,8 @@ from spade.behaviour import OneShotBehaviour
 from spade.message import Message
 from spade.template import Template
 
+
+
 class Drone(Agent):
     def __init__(self, jid, password, capacity, autonomy, velocity, initialPos, numCenters):
         super().__init__(jid, password)
@@ -15,25 +17,47 @@ class Drone(Agent):
 
     async def setup(self):
         print(f"Drone {self.jid} is ready")
+        self.add_behaviour(self.Presence())
 
 
-    class RecvBehav(OneShotBehaviour):
+    # class RecvBehav(OneShotBehaviour):
+    #     async def run(self):
+    #         msg = await self.receive(timeout=20) # wait for a message for 10 seconds
+    #         if msg:
+    #             print("Message received with content: {}".format(msg.body))
+    #         else:
+    #             print("Did not received any message after 10 seconds")
+
+    #         # stop agent from behaviour
+    #         await self.agent.stop()
+
+    # async def setup(self):
+    #     print("ReceiverAgent started")
+    #     b = self.RecvBehav()
+    #     template = Template()
+    #     template.set_metadata("performative", "query")
+    #     self.add_behaviour(b, template)
+
+
+    class Presence(OneShotBehaviour):
+        def on_available(self, jid, stanza):
+            print("[{}] Agent {} is available.".format(self.agent.name, jid.split("@")[0]))
+
+        def on_subscribed(self, jid):
+            print("[{}] Agent {} has accepted the subscription.".format(self.agent.name, jid.split("@")[0]))
+            print("[{}] Contacts List: {}".format(self.agent.name, self.agent.presence.get_contacts()))
+
+        def on_subscribe(self, jid):
+            print("[{}] Agent {} asked for subscription. Let's aprove it.".format(self.agent.name, jid.split("@")[0]))
+            self.presence.approve(jid)
+            self.presence.subscribe(jid)
+
         async def run(self):
-            msg = await self.receive(timeout=20) # wait for a message for 10 seconds
-            if msg:
-                print("Message received with content: {}".format(msg.body))
-            else:
-                print("Did not received any message after 10 seconds")
-
-            # stop agent from behaviour
-            await self.agent.stop()
-
-    async def setup(self):
-        print("ReceiverAgent started")
-        b = self.RecvBehav()
-        template = Template()
-        template.set_metadata("performative", "query")
-        self.add_behaviour(b, template)
+            self.presence.set_available()
+            print('oii')
+            self.presence.on_subscribe = self.on_subscribe
+            self.presence.on_subscribed = self.on_subscribed
+            self.presence.on_available = self.on_available
 
 
 
--- environment.py
+++ environment.py
@@ -27,13 +27,15 @@ class Environment(Agent):
             await drone.start()
             drones.append(drone)
         
-        print(drones)
+        #print(drones)
 
-        center1 = Center("center1@localhost", "password", df_center1, len(df_drones))
-        await center1.start()
+        print(drones[0].presence.state)
 
-        center2 = Center("center2@localhost", "password", df_center2, len(df_drones))
-        await center2.start()
+        # center1 = Center("center1@localhost", "password", df_center1, len(df_drones))
+        # await center1.start()
+
+        # center2 = Center("center2@localhost", "password", df_center2, len(df_drones))
+        # await center2.start()
     
     async def setup(self):
         print(f"Environment {self.jid} is ready")
